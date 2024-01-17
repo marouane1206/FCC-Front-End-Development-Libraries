d4c07cd5cd49bc8a8a6077560f586050880df408
marouane1206 <narouane@bii-system.com>
2024-01-17 12:24:19 +0100

    edited the drum styles

--- markdown-previewer-app/src/App.js
+++ markdown-previewer-app/src/App.js
@@ -78,13 +78,13 @@ function App() {
 const defaultMarkdown =
   "# Welcome to my React Markdown Previewer!\n\n## This is a sub-heading...\n### And here's some other cool stuff:\n\nHere's some code,`<div></div>`,between 2 backticks.\n\n```\n// this is multi-line code:\n\nfunction anotherExample(firstLine, lastLine) {\n  if (firstLine == '```' && lastLine == '```') {\n    return multiLineCode;\n  }\n}\n```\n\nYou can also make text **bold**... whoa!\nOr _italic_.\nOr... wait for it... **_both!_**\nAnd feel free to go crazy ~~crossing stuff out~~.\n\nThere's also [links](https://www.freecodecamp.org), and\n> Block Quotes!\n\nAnd if you want to get really crazy, even tables:\n\nWild Header | Crazy Header | Another Header?\n------------ | ------------- | -------------\nYour content can | be here, and it | can be here....\nAnd here. | Okay. | I think we get it.\n\n- And of course there are lists.\n  - Some are bulleted.\n     - With different indentation levels.\n        - That look like this.\n\n\n1. And there are numbered lists too.\n1. Use just 1s if you want!\n1. And last but not least, let's not forget embedded images:\n\n![Markdown Logo](https://miro.medium.com/v2/resize:fit:786/format:webp/1*eZ7YPTqzcyFVoQxIOIQ9kQ.png)\n";
 
-function getRandomColor() {
+const getRandomColor = () => {
   const letters = "0123456789ABCDEF";
   let color = "#";
   for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
-}
+};
 
 export default App;
